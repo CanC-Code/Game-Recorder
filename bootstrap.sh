49e0ae231785d5fbd5a60fa27650e727339c3a3a
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-07-20 22:06:22 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Generates the Gradle wrapper files required for CI/CD builds
-gradle wrapper --gradle-version 8.9 --distribution-type bin
+# Generates the Gradle wrapper files required for CI/CD builds (using Gradle 8.11.1 for AGP compatibility)
+gradle wrapper --gradle-version 8.11.1 --distribution-type bin
 chmod +x gradlew
